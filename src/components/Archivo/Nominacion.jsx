e6622b044a900944897f6b477490e534f68ecdf4
UtopicRay <ray2021@gmail.com>
2024-01-28 02:33:19 -0500

    'Nominacion Casi-Completo'

--- src/components/Archivo/Nominacion.jsx
+++ src/components/Archivo/Nominacion.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Cartilla } from "./Cartilla";
+import { CartillaPequeña } from "./CartillaPequeña";
 
 export default function Nominacion() {
   const [target, setTarget] = useState(1);
@@ -8,8 +9,8 @@ export default function Nominacion() {
     setTarget(x);
   };
   return (
-    <div className="grid grid-cols-2 mt-10 gap-10 w-full">
-      <ul className="md:[&&>li]:text-xl [&&>li]:text-sm">
+    <div className="grid md:grid-cols-2 mt-10 gap-10 w-full grid-cols-1">
+      <ul className="md:[&&>li]:text-xl">
         <li className="bg-[#000527] w-full">
           <div className="flex flex-grow justify-between w-full">
             <span className="m-2 font-semibold">Categoría</span>
@@ -28,7 +29,10 @@ export default function Nominacion() {
             <span className="m-2 font-light">Ibai</span>
           </div>
         </li>
-        <li onClick={(e, x) => ChangeTarget(e, 2)} className="bg-[#000527] list">
+        <li
+          onClick={(e, x) => ChangeTarget(e, 2)}
+          className="bg-[#000527] list"
+        >
           <div className="flex flex-grow justify-between w-full">
             <span className="m-2 font-light">Serie de contenido del año</span>
             <span className="m-2 font-light">Tortillaland</span>
@@ -36,7 +40,7 @@ export default function Nominacion() {
         </li>
         <li
           onClick={(e, x) => ChangeTarget(e, 3)}
-          className="list revelacion"
+          className="list"
           style={{
             background: "linear-gradient(to left,#0f2646  -15%,#1682c7)",
           }}
@@ -46,7 +50,10 @@ export default function Nominacion() {
             <span className="m-2 font-light">elXokas</span>
           </div>
         </li>
-        <li onClick={(e, x) => ChangeTarget(e, 4)} className="bg-[#000527] list">
+        <li
+          onClick={(e, x) => ChangeTarget(e, 4)}
+          className="bg-[#000527] list"
+        >
           <div className="flex flex-grow justify-between w-full">
             <span className="m-2 font-light">Evento del año</span>
             <span className="m-2 font-light">La velada del año</span>
@@ -64,7 +71,10 @@ export default function Nominacion() {
             <span className="m-2 font-light">Fernanfloo</span>
           </div>
         </li>
-        <li onClick={(e, x) => ChangeTarget(e, 6)} className="bg-[#000527] list">
+        <li
+          onClick={(e, x) => ChangeTarget(e, 6)}
+          className="bg-[#000527] list"
+        >
           <div className="flex flex-grow justify-between w-full">
             <span className="m-2 font-light">Talk show del año</span>
             <span className="m-2 font-light">The Wild Project</span>
@@ -84,7 +94,7 @@ export default function Nominacion() {
         </li>
         <li
           onClick={(e, x) => ChangeTarget(e, 8)}
-          className="list bg-[#000527] jagger"
+          className="list bg-[#000527]"
         >
           <div className="flex flex-grow justify-between w-full">
             <span className="m-2 font-light">Jägger del año</span>
@@ -93,7 +103,7 @@ export default function Nominacion() {
         </li>
         <li
           onClick={(e, x) => ChangeTarget(e, 9)}
-          className="list steamer-IRL"
+          className="list"
           style={{
             background: "linear-gradient(to left,#0f2646  -15%,#1682c7)",
           }}
@@ -105,7 +115,7 @@ export default function Nominacion() {
         </li>
         <li
           onClick={(e, x) => ChangeTarget(e, 10)}
-          className="bg-[#000527] list caster"
+          className="bg-[#000527] list"
         >
           <div className="flex flex-grow justify-between w-full">
             <span className="m-2 font-light">Caster del año</span>
@@ -120,11 +130,17 @@ export default function Nominacion() {
           }}
         >
           <div className="flex flex-grow justify-between w-full">
-            <span className="m-2 font-light"> Reportero/a de esports del año</span>
+            <span className="m-2 font-light">
+              {" "}
+              Reportero/a de esports del año
+            </span>
             <span className="m-2 font-light">Cristinini</span>
           </div>
         </li>
-        <li onClick={(e, x) => ChangeTarget(e, 12)} className="bg-[#000527] list">
+        <li
+          onClick={(e, x) => ChangeTarget(e, 12)}
+          className="bg-[#000527] list"
+        >
           <div className="flex flex-grow justify-between w-full">
             <span className="m-2 font-light">Jugador/a de esports del año</span>
             <span className="m-2 font-light">Mixwell</span>
@@ -142,7 +158,10 @@ export default function Nominacion() {
             <span className="m-2 font-light">AgenteMaxo</span>
           </div>
         </li>
-        <li onClick={(e, x) => ChangeTarget(e, 14)} className="bg-[#000527] list">
+        <li
+          onClick={(e, x) => ChangeTarget(e, 14)}
+          className="bg-[#000527] list"
+        >
           <div className="flex flex-grow justify-between w-full">
             <span className="m-2 font-light">Baile del año</span>
             <span className="m-2 font-light">Papi Gavi</span>
@@ -160,7 +179,10 @@ export default function Nominacion() {
             <span className="m-2 font-light">Agustin51</span>
           </div>
         </li>
-        <li onClick={(e, x) => ChangeTarget(e, 16)} className="bg-[#000527] list">
+        <li
+          onClick={(e, x) => ChangeTarget(e, 16)}
+          className="bg-[#000527] list"
+        >
           <div className="flex flex-grow justify-between w-full">
             <span className="m-2 font-light">Fail del año</span>
             <span className="m-2 font-light">Ibai</span>
@@ -179,9 +201,7 @@ export default function Nominacion() {
           </div>
         </li>
       </ul>
-      <div className="flex flex-wrap gap-10 justify-center items-end">
-        <Cartilla objet={target}/>
-      </div>
+      <Cartilla objet={target} />
     </div>
   );
 }
