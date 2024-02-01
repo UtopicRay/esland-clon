36f539b052780dc17ceaf4f6ac1a2fa6bb05e36c
UtopicRay <ray2021@gmail.com>
2024-02-01 10:32:20 -0500

    'Eliminando Vercel Output'

--- .vercel/output/functions/_render.func/media/ray/New Volume/Escuela/Proyectos del Curso Completo de HTML/React-Proyect/prueba-astro/esland-clon/.vercel/output/_functions/chunks/Archivo_Z_31-N1b.mjs
+++
@@ -1,6 +0,0 @@
-export { renderers } from '../renderers.mjs';
-export { onRequest } from '../_empty-middleware.mjs';
-
-const page = () => import('./pages/Archivo_8ohdLjgM.mjs').then(n => n.A);
-
-export { page };
